fbfbd259e2a44aa7eddf8c21b1cab79be0f6d9dd
Régis Belson <regis@evaneos.com>
2016-10-13 16:29:12 +0200

    Add test for five

---
+++ src/RomanNumeral.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Arola\RomanNumerals;
+
+class RomanNumeral
+{
+    public function __construct($int)
+    {
+    }
+
+    public function toString()
+    {
+        return 'I';
+    }
+}
