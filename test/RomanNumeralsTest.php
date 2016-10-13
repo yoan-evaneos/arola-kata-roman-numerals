6719d0cc73e51690102040772928b7b23d76d824
Yoan Haouzi <yoan.haouzi@evaneos.com>
2016-10-13 17:04:04 +0200

    <test> Implement some tests

--- test/RomanNumeralsTest.php
+++ test/RomanNumeralsTest.php
@@ -10,7 +10,8 @@ class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function testItShouldReturnOne() {
+    public function testItShouldReturnOne()
+    {
         $numeral = new RomanNumeral(1);
         $this->assertEquals('I', $numeral->toString());
     }
@@ -18,7 +19,8 @@ class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function testItShouldReturnFive() {
+    public function testItShouldReturnFive()
+    {
         $numeral = new RomanNumeral(5);
         $this->assertEquals('V', $numeral->toString());
     }
@@ -26,16 +28,24 @@ class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function testItShouldReturnTen() {
+    public function testItShouldReturnTen()
+    {
         $numeral = new RomanNumeral(10);
         $this->assertEquals('X', $numeral->toString());
     }
 
-    /**
+    /*
      * @test
      */
-    public function testItShouldReturnFour() {
-        $numeral = new RomanNumeral(4);
-        $this->assertEquals('IV', $numeral->toString());
+    public function testItIsARomanNumber()
+    {
+        $random = rand(11, 2000);
+        $numeral = new RomanNumeral($random);
+
+        $this->assertRegExp(
+            '/[IVXLCDM]+/',
+            $numeral->toString(),
+            sprintf('random %s does not match with roman numbers', $random)
+        );
     }
 }
