17686350430c91d6943da2bb7a3e2c8e54678dea
Yoan Haouzi <yoan.haouzi@evaneos.com>
2016-10-13 17:37:41 +0200

    <test> Implement algorithm

---
+++ src/RomanNumeralInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoanhaouzi
+ * Date: 13/10/2016
+ * Time: 17:16
+ */
+
+namespace Arola\RomanNumerals;
+
+interface RomanNumeralInterface{
+
+    /**
+     * @param $int
+     * @return bool
+     */
+    public function inRange($int);
+
+    public function getValue();
+
+    public function toString();
+}
---
+++ src/RomanNumeralOne.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoanhaouzi
+ * Date: 13/10/2016
+ * Time: 17:19
+ */
+
+namespace Arola\RomanNumerals;
+
+
+class RomanNumeralOne implements RomanNumeralInterface
+{
+
+    public function getValue() {
+        return 1;
+    }
+
+    public function toString()
+    {
+        return 'I';
+    }
+
+    /**
+     * @param $int
+     * @return bool
+     */
+    public function inRange($int)
+    {
+        return $int >= 1 && $int <= 3;
+    }
+}
